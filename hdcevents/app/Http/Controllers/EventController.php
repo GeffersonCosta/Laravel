265a7f03ed0e1e35b1a5bec48466141cf1ab0f34
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-10-27 09:19:15 +0000

    add function store

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -3,33 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
+use App\Models\Event;
+use App\Http\Controllers\save;
 class EventController extends Controller
 {
    public function index(){
-    $nome = "Gefferson";
-    $arr = [1,2,3,4,7,8,9];
-    return view('welcome', compact('nome', 'arr'));
-    //return view('welcome', ['nome' => $nome]);
+    $participante = 1;
+    $events = Event::all();
+
+    return view('welcome', compact('events', 'participante'));
+   }
+
+   public function store(Request $request){
+        $events = new Event();
+          
+        $events['title'] = $request['title'];
+        $events['city'] = $request['city'];
+        $events['private'] = $request['private'];
+        $events['description'] = $request['description'];
+        $events -> save();
+        return redirect('/');
    }
 
 
    public function create(){
+
+    // DB::table('events')->insert([
+    //     'title' => 'Evento teste',
+    //     'description' => 'alguma descrição',
+    //     'city' => 'Floripa',
+    //     'private' => 0,
+    // ]);
+    // DB::table('events')->insert([
+    //     'title' => 'Evento de JavaScript',
+    //     'description' => 'aprender muito JS',
+    //     'city' => 'São paulo',
+    //     'private' => 0,
+    // ]);
+    // DB::table('events')->insert([
+    //     'title' => 'Evento PHP',
+    //     'description' => 'Aprender PHP',
+    //     'city' => 'São José',
+    //     'private' => 0,
+    // ]);
+
+
+
      return view('events.create');
-   }
+    }
 
    public function contato(){
     return view('contato');
    }
 
-   public function produtos(){
-    $busca = request('search');
-    return view('produtos', compact('busca'));
-   }
 
-   public function produto($id){
-    return view('produto', compact('id'));
-   }
 
 }
 
