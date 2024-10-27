a9655dd3ff7052de50bf659aca04ddcc4bfb108e
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-10-27 09:23:40 +0000

    created form

--- hdcevents/resources/views/events/create.blade.php
+++ hdcevents/resources/views/events/create.blade.php
@@ -2,6 +2,29 @@
 @section('title', 'Criar Evento')
 @section('content')
 
-<h1>Pagina criar evento</h1>
+
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie o seu evento</h1>
+    <form action="/events" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+
+            <label for="title">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+
+            <label for="title">O evento é privado?</label>
+            <select name="private" id="" class="form-control">
+                <option value="1">Sim</option>
+                <option value="0">Nao</option>
+            </select>
+            <label for="title">Descrição:</label>
+            <textarea name="description" id="description" class="form-control" placeholder="Descreva o tipo de evento"></textarea>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Criar evento">
+    </form>
+</div>
+
 
 @endsection
