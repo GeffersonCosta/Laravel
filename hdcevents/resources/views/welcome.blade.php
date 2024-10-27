cc27af3d75610dbbf312639fde8aeb20909a8e16
Gefferson Costa <81829446+GeffersonCosta@users.noreply.github.com>
2024-10-27 09:24:41 +0000

    update file welcome

--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -1,19 +1,37 @@
        @extends('layouts.main')
        @section('title', 'Projeto')
        @section('content')
-       <h1>Hello World</h1>
-        <a id="link" href="/produtos">Pagina produtos</a><br><br>
-        <a href="/contato">Pagina contatos</a>
         {{-- Com o blade pode escrever comentarios e não são interpretados pelo navegador e não aparece no inspecionar--}}
-       @if($nome == 'Henrique')
-        <p>O nome é igual</p>
-       @else
-        <p>O nome não é igual </p>
-       @endif
+       <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+        <form action="">
+            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+        </form>
+       </div>
+       <div id="events-container" class="col-md-12">
+        <h2>Próximos Eventos</h2>
+        <p class="subtitle">Veja os eventos dos próximos dias</p>
+        <div id="cards-container" class="row">
+          @foreach ($events as $event)
+            <div class="card col-md-3">
+                <img src="/imagens/foto1.jpg" alt="{{$event['title']}}">
+                <div class="card-body">
+                    <p class="card-date">10/09/2024</p>
+                    <h5 class="card-title">{{$event['title']}}</h5>
+                    <p class="card-participants">{{$participante++}}-Participantes</p>
+                    <p class="card-participants">{{$event['description']}}</p>
+                    <a href="#" class="btn btn-primary">Saber mais</a>
+                </div>
+            </div>
+          @endforeach
+        </div>
+       </div>
 
-       @foreach ($arr as $elemento)
-        <p>{{$elemento}}</p>
-       @endforeach
-       <img src="/imagens/foto1.jpg" alt="">
+
+
+
+
+
        @endsection
+
 
